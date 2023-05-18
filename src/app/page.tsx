c785659c2216d5da45107c61725ee5186caf3fa3
Lars Grammel <lars.grammel@gmail.com>
2023-05-18 17:36:57 +0200

    Reset page style and add basic inputs.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,85 +1,51 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { useState } from "react";
+import { TextField, Button } from "@mui/material";
+import styles from "./page.module.css";
 
 export default function Home() {
+  const [jobDescription, setJobDescription] = useState("");
+  const [resumeFile, setResumeFile] = useState<File | null>(null);
+
+  const handleResumeFileChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setResumeFile(event.target.files[0]);
+    } else {
+      setResumeFile(null);
+    }
+  };
+
+  const handleGenerateCoverLetter = () => {
+    // TODO: Implement cover letter generation logic
+    console.log("Generating cover letter...");
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <TextField
+        id="job-description"
+        label="Job Description"
+        placeholder="Enter job description here"
+        multiline
+        rows={5}
+        variant="outlined"
+        value={jobDescription}
+        onChange={(event) => {
+          setJobDescription(event.target.value);
+        }}
+      />
+      <input
+        accept="application/pdf"
+        id="resume-file"
+        type="file"
+        onChange={handleResumeFileChange}
+      />
+      <Button variant="contained" onClick={handleGenerateCoverLetter}>
+        Generate Cover Letter
+      </Button>
     </main>
-  )
+  );
 }
